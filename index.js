4c8b3601687f124c261febb474db70ba5b7e5fc6
Aliyv019 <tahiraliyev2006@gmail.com>
2024-05-21 20:30:17 +0400

    task elave olundu

---
+++ index.js
@@ -0,0 +1,57 @@
+// var a = prompt("stringi daxil edin")
+
+// var len = a.length
+
+// alert(len)
+
+// var name = prompt("ad daxil edin")
+
+// if (name.length >15){
+//     alert("duzgun ad yazin")
+// }
+// else{
+//     alert(name)
+// }
+
+// var a ='TaHir'
+
+// alert(a.toUpperCase())
+
+
+// var a = "Tahir".toLowerCase().trim()
+// alert(a.charAt(1))
+
+// var a = "Salam necesoooz"
+// alert(a.replace("necesoooz", "necesiniz?"))
+
+
+// var a = prompt("adinizi qeyd edin")
+// alert(`${a} xos geldiniz`)
+
+// var a ="Salam bu gun gununuz nece kecib?"
+// alert(a.replaceAll("a", 'c'))
+
+// var a=prompt("sozu daxil edin")
+// if (a.length < 5){
+//     alert("Data is not enough")
+// }
+// else{
+//     alert(a.replaceAll("a", "k"))
+// }
+// hansisa indexi bosluq olsa ekrana error ver
+// var a = prompt("Enter your text")
+// if (a.includes(" ")){
+//     alert("this is an error")
+// }
+// else{
+//     alert(a)
+// }
+
+// var a =prompt("enter your text")
+// var b=a.replaceAll(" ", "")
+// if(a.length==b.length){
+//     alert(a)
+// }
+// else{
+//     alert("error")
+// }
